15cd11a64f6175727a51788f9c5f77985d2af4a2
jankilighthouse <jankipatel6890@gmail.com>
2018-09-04 00:07:41 +0000

    avatar_url completed

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,32 @@
 var request = require('request');
+ var secret = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secret.GITHUB_TOKEN
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+
+      var parsed = JSON.parse(body);
+});
 }
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  var parse=JSON.parse(result);
+
+  for(var i=0;i<parse.length;i++)
+  {
+    var output_url = parse[i].avatar_url;
+          console.log(output_url);
+
+
+  }
+
+});
---
+++ secrets.js
@@ -0,0 +1,3 @@
+module.exports = {
+  GITHUB_TOKEN: "71b642f2280c16eea51890cba64a2955a5a1e16e"
+};
